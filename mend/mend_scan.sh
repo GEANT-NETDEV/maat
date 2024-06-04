0b7cf028fa20cf994e8e9dc8d4c6e125f0d566cf
Marcin Siuda <msiuda@man.poznan.pl>
2024-06-04 13:21:21 +0200

    Mend configuration

--- mend/mend_scan.sh
+++ mend/mend_scan.sh
@@ -4,4 +4,4 @@ echo "Downloading Mend agent..."
 curl -LJO https://github.com/whitesource/unified-agent-distribution/releases/latest/download/wss-unified-agent.jar
 
 echo "Running Mend scan..."
-java -jar wss-unified-agent.jar -apiKey ${API_KEY} -userKey ${USER_KEY} -projectVersion ${VERSION} -productVersion ${VERSION} -c mend.config -d ../
+java -jar wss-unified-agent.jar -apiKey ${API_KEY} -userKey ${USER_KEY} -c mend.config -d ../
