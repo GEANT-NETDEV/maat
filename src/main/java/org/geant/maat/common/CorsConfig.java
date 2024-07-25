16835474c38b7576c2a40225e80329d34d911815
Marcin Siuda <msiuda@man.poznan.pl>
2024-07-25 11:53:39 +0200

    Modified CORS

--- src/main/java/org/geant/maat/common/CorsConfig.java
+++ src/main/java/org/geant/maat/common/CorsConfig.java
@@ -23,7 +23,7 @@ public class CorsConfig implements Filter {
         response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
         //response.setHeader("Access-Control-Allow-Origin", request.getHeader("Host"));
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         //response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
 
