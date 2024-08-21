226a0490531a3cf26b9ce727e6d941f2e9e0de16
Marcin Siuda <msiuda@man.poznan.pl>
2024-08-21 11:02:55 +0200

    Changed CORS configuration

--- src/main/java/org/geant/maat/common/DisableSecurityConfiguration.java
+++ src/main/java/org/geant/maat/common/DisableSecurityConfiguration.java
@@ -1,22 +1,62 @@
 package org.geant.maat.common;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @ConditionalOnProperty(name = "keycloak.enabled", havingValue = "false")
 public class DisableSecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf()
-                .disable()
-                .authorizeRequests()
-                .anyRequest()
-                .permitAll();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        return new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration config = new CorsConfiguration();
+
+                String origin = request.getHeader("Origin");
+
+                if (origin != null) {
+                    config.setAllowedOrigins(List.of(origin));
+                }
+
+                config.setAllowedMethods(List.of("GET", "POST", "PATCH", "DELETE", "OPTIONS"));
+                config.setAllowedHeaders(List.of(
+                        "Content-Type",
+                        "Origin",
+                        "Accept",
+                        "X-Requested-With",
+                        "remember-me",
+                        "Authorization"
+                ));
+                config.setAllowCredentials(true);
+
+                return config;
+            }
+        };
     }
 }
