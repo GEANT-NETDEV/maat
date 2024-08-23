caf1e58c03d41427a01b1af9029f09ff7d064af8
Marcin Siuda <msiuda@man.poznan.pl>
2024-08-23 12:59:22 +0200

    Authorization restrictions on rest methods

--- src/main/java/org/geant/maat/common/KeycloakSecurityConfig.java
+++ src/main/java/org/geant/maat/common/KeycloakSecurityConfig.java
@@ -1,8 +1,10 @@
 package org.geant.maat.common;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,7 +14,17 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @ConditionalOnProperty(name = "keycloak.enabled", havingValue = "true", matchIfMissing = true)
 public class KeycloakSecurityConfig {
+    @Value("${spring.security.role.get_only}")
+    private String getOnlyRole;
+    @Value("${spring.security.role.post_only}")
+    private String postOnlyRole;
+    @Value("${spring.security.role.delete_only}")
+    private String deleteOnlyRole;
+    @Value("${spring.security.role.patch_only}")
+    private String patchOnlyRole;
+
     private final KeycloakJwtTokenConverter keycloakJwtTokenConverter;
+
 
     public KeycloakSecurityConfig(TokenConverterProperties properties) {
         JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter
@@ -26,16 +38,20 @@ public class KeycloakSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests()
-                .anyRequest()
-                .authenticated();
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.GET).hasRole(getOnlyRole)
+                        .requestMatchers(HttpMethod.POST).hasRole(postOnlyRole)
+                        .requestMatchers(HttpMethod.DELETE).hasRole(deleteOnlyRole)
+                        .requestMatchers(HttpMethod.PATCH).hasRole(patchOnlyRole)
+                        .anyRequest()
+                        .authenticated());
         http
-                .oauth2ResourceServer()
-                .jwt()
-                .jwtAuthenticationConverter(keycloakJwtTokenConverter);
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(keycloakJwtTokenConverter)));
         http
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
         return http.build();
     }
 }
