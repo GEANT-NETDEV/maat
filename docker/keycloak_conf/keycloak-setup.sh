d7ac48fd3e83ae5df3ca29d559e8ff3fa8430337
Marcin Siuda <msiuda@man.poznan.pl>
2024-11-07 15:34:45 +0100

    Modification in docker files

--- docker/keycloak_conf/keycloak-setup.sh
+++ docker/keycloak_conf/keycloak-setup.sh
@@ -3,8 +3,8 @@
 /opt/keycloak/bin/kcadm.sh create realms -s realm=MaatRealm -s enabled=true
 /opt/keycloak/bin/kcadm.sh create users -r MaatRealm -s username=$KEYCLOAK_USER -s enabled=true -s email=account@mail.com -s firstName=firstName -s lastName=lastName -s emailVerified=true
 /opt/keycloak/bin/kcadm.sh set-password -r MaatRealm --username $KEYCLOAK_USER --new-password $KEYCLOAK_PASS
-/opt/keycloak/bin/kcadm.sh create clients -r MaatRealm -s clientId=$KEYCLOAK_CLIENT_ID -s enabled=true -s clientAuthenticatorType=client-secret -s secret=$KEYCLOAK_CLIENT_SECRET -s standardFlowEnabled=true -s redirectUris=[\"http://172.18.187.252:8090\"]
-/opt/keycloak/bin/kcadm.sh create clients -r MaatRealm -s clientId=maat-account -s enabled=true -s clientAuthenticatorType=client-secret -s secret=$KEYCLOAK_CLIENT_SECRET -s serviceAccountsEnabled=true -s standardFlowEnabled=false
+/opt/keycloak/bin/kcadm.sh create clients -r MaatRealm -s clientId=$KEYCLOAK_CLIENT_ID_FOR_USERS -s enabled=true -s clientAuthenticatorType=client-secret -s secret=$KEYCLOAK_CLIENT_SECRET -s standardFlowEnabled=true -s redirectUris="[\"http://${KEYCLOAK_HOST}*\", \"https://${KEYCLOAK_HOST}*\"]"
+/opt/keycloak/bin/kcadm.sh create clients -r MaatRealm -s clientId=$KEYCLOAK_CLIENT_ID_FOR_CLIENTS -s enabled=true -s clientAuthenticatorType=client-secret -s secret=$KEYCLOAK_CLIENT_SECRET -s serviceAccountsEnabled=true -s standardFlowEnabled=false
 /opt/keycloak/bin/kcadm.sh create roles -r MaatRealm -s name=maatuser
 /opt/keycloak/bin/kcadm.sh add-roles --uusername $KEYCLOAK_USER --rolename maatuser -r MaatRealm
 /opt/keycloak/bin/kcadm.sh add-roles --uusername service-account-maat-account --rolename maatuser -r MaatRealm
