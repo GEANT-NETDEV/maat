1d71b59bf4bc9ab2c709f20435a418947f18e448
Marcin Siuda <msiuda@man.poznan.pl>
2026-04-08 13:12:34 +0200

    Added the ability to log and notify about the user who executed actions in Maat

--- src/main/java/org/geant/maat/common/DisableSecurityConfiguration.java
+++ src/main/java/org/geant/maat/common/DisableSecurityConfiguration.java
@@ -51,6 +51,7 @@ public class DisableSecurityConfiguration {
                         "Accept",
                         "X-Requested-With",
                         "remember-me",
+                        CurrentUserResolver.AUTHENTICATED_USER_HEADER,
                         "Authorization"
                 ));
                 config.setAllowCredentials(true);
